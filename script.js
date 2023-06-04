9c4da039f7b364572371b013ba57e38bdd17c386
la-ser <77540680+la-ser@users.noreply.github.com>
2023-06-04 12:39:39 +0200

    1

--- script.js
+++ script.js
@@ -8,15 +8,12 @@ const scrollContainer = () => {
 
 const goToTop = () => {
   document.body.scrollIntoView({
-    behavior: "smooth"
+    behavior: "smooth",
   });
 };
 
 document.addEventListener("scroll", () => {
-  const scrolledPercentage =
-    (scrollContainer().scrollTop /
-      (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
-    100;
+  const scrolledPercentage = (scrollContainer().scrollTop / (scrollContainer().scrollHeight - scrollContainer().clientHeight)) * 100;
 
   if (scrollContainer().scrollTop > showOnPx) {
     backToTopButton.classList.remove("hidden");
@@ -25,4 +22,27 @@ document.addEventListener("scroll", () => {
   }
 });
 
-backToTopButton.addEventListener("click", goToTop);
+// backToTopButton.addEventListener("click", goToTop);
+
+var images = [
+  "background/website.png",
+  "background/2.png",
+  "background/3.png",
+  "background/4.png",
+  "background/5.png",
+  "background/6.png",
+  "background/7.png",
+  "background/8.png",
+  "background/9.png",
+  "background/10.png",
+];
+var currentIndex = 0;
+
+function changeBackgroundImageForward() {
+  currentIndex = (currentIndex + 1) % images.length;
+  document.body.style.backgroundImage = "url('" + images[currentIndex] + "')";
+}
+function changeBackgroundImageBackward() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  document.body.style.backgroundImage = "url('" + images[currentIndex] + "')";
+}

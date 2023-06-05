ea758d1b873e0cb586f096c9f5b90fd4e38c2a03
la-ser <77540680+la-ser@users.noreply.github.com>
2023-06-05 21:48:54 +0200

    1

--- script.js
+++ script.js
@@ -46,3 +46,91 @@ function changeBackgroundImageBackward() {
   currentIndex = (currentIndex - 1 + images.length) % images.length;
   document.body.style.backgroundImage = "url('" + images[currentIndex] + "')";
 }
+
+// /* image hover */
+// const luxury_district = document.getElementById("luxury-district");
+
+// luxury_district.addEventListener("mouseover", function () {
+//   setTimeout(function () {
+//     document.body.classList.add("luxury-district");
+//   }, 300);
+// });
+
+// luxury_district.addEventListener("mouseout", function () {
+//   setTimeout(function () {
+//     document.body.classList.remove("luxury-district");
+//   }, 500);
+// });
+
+// /* subway */
+// const subway_and_more = document.getElementById("subway-and-more");
+// subway_and_more.addEventListener("mouseover", function () {
+//   setTimeout(function () {
+//     document.body.classList.add("subway-and-more");
+//   }, 300);
+// });
+
+// subway_and_more.addEventListener("mouseout", function () {
+//   setTimeout(function () {
+//     document.body.classList.remove("subway-and-more");
+//   }, 500);
+// });
+
+// /* new york */
+// const little_new_york = document.getElementById("little-new-york");
+// little_new_york.addEventListener("mouseover", function () {
+//   setTimeout(function () {
+//     document.body.classList.add("little-new-york");
+//   }, 300);
+// });
+
+// little_new_york.addEventListener("mouseout", function () {
+//   setTimeout(function () {
+//     document.body.classList.remove("little-new-york");
+//   }, 500);
+// });
+
+// /* cafe */
+// const beach_and_cafe = document.getElementById("beach-and-cafe");
+// beach_and_cafe.addEventListener("mouseover", function () {
+//   setTimeout(function () {
+//     document.body.classList.add("beach-and-cafe");
+//   }, 300);
+// });
+
+// beach_and_cafe.addEventListener("mouseout", function () {
+//   setTimeout(function () {
+//     document.body.classList.remove("beach-and-cafe");
+//   }, 500);
+// });
+const body = document.body;
+const imageElements = document.querySelectorAll(".hover-img");
+const imageCache = [];
+
+// Preload images
+imageElements.forEach(function (element) {
+  const bg = element.getAttribute("data-bg");
+  if (bg) {
+    const img = new Image();
+    img.src = bg;
+    imageCache.push(img);
+  }
+});
+
+imageElements.forEach(function (element) {
+  element.addEventListener("mouseover", function () {
+    const bg = element.getAttribute("data-bg");
+    if (bg) {
+      body.addEventListener("transitionend", function () {
+        body.style.backgroundImage = `url('${bg}')`;
+      });
+
+      body.classList.add("image-transition");
+    }
+  });
+
+  element.addEventListener("mouseout", function () {
+    body.style.backgroundImage = "";
+    body.classList.remove("image-transition");
+  });
+});
